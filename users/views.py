d5675b8cff392a36be06333db04270ae6a7221ff
Pavel Anikin <PavelDidko@gmail.com>
2025-11-01 08:44:48 +0300

    отладка перехода после регистрации на login

--- users/views.py
+++ users/views.py
@@ -6,7 +6,7 @@ from django.utils.translation import gettext_lazy as _
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.contrib.auth.models import User
-from django.urls import reverse_lazy
+# from django.urls import reverse_lazy
 
 from .forms import RegisterForm, CustomUserChangeForm
 
@@ -56,9 +56,21 @@ class UsersListView(ListView):
 class UsersCreateView(FormLoggerMixin, SuccessMessageMixin, CreateView):
     form_class = RegisterForm
     template_name = "users/create.html"
-    success_url = reverse_lazy("login")
+    success_url = "/login/"
     success_message = _("Пользователь успешно зарегистрирован!")
     log_message = "Создать пользователя: {obj.username}"
+
+    # код отладки после удалить
+    def form_valid(self, form):
+        print("Форма валидна, сохраняем объект")  # Отладка
+        response = super().form_valid(form)
+        print("Перенаправление на:", self.get_success_url())
+        return response
+
+    def form_invalid(self, form):
+        print("Форма невалидна. Ошибки:", form.errors)  # Отладка
+        print("Данные:", form.data)
+        return super().form_invalid(form)
 
 
 class UsersUpdateView(
