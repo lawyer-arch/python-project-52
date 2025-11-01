5bb20070403f15272add6e898c0f13750b94385d
Pavel Anikin <PavelDidko@gmail.com>
2025-11-01 14:05:20 +0300

    добавил поля паролей в форму редактирования

--- users/forms.py
+++ users/forms.py
@@ -1,5 +1,6 @@
-from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django import forms
 
 
 class RegisterForm(UserCreationForm):
@@ -14,13 +15,35 @@ class RegisterForm(UserCreationForm):
         )
 
 
-class CustomUserChangeForm(UserChangeForm):
-    password = None
-
-    class Meta(UserChangeForm.Meta):
+class CustomUserChangeForm(UserCreationForm):
+    # Добавляем вручную оба поля паролей
+    password1 = forms.CharField(
+        label="Пароль",
+        strip=False,
+        widget=forms.PasswordInput(attrs={'autocomplete': 'new-password'}),
+    )
+    password2 = forms.CharField(
+        label="Подтверждение пароля",
+        widget=forms.PasswordInput(attrs={'autocomplete': 'new-password'}),
+        strip=False,
+        help_text="Введите тот же пароль ещё раз.",
+    )
+    
+    class Meta(UserCreationForm.Meta):
         model = User
-        fields = (
-            "first_name",
-            "last_name",
-            "username",
-        )
+        fields = ("first_name", "last_name", "username")
+
+    def clean_password2(self):
+        """Проверяем совпадение введённых паролей"""
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError("Пароли не совпадают.")
+        return password2
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data["password1"])
+        if commit:
+            user.save()
+        return user
