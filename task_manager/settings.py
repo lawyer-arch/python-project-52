e03f84798c40fdc6d8d0ae363d0896bc549824cf
Pavel Anikin <PavelDidko@gmail.com>
2025-11-02 19:49:48 +0300

    удалил ststic

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -140,11 +140,6 @@ STATIC_URL = '/static/'
 # Директория, где собираются статические файлы
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-# Пути к директориям с вашими собственными статическими файлами
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
