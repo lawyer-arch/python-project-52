0c75a874dea4b526324b9a1a6d0f9186a5233f4c
Pavel Anikin <PavelDidko@gmail.com>
2025-11-03 17:56:33 +0300

    добавли в tasks форму forms.py

--- tasks/forms.py
+++ tasks/forms.py
@@ -8,9 +8,12 @@ class TaskForm(forms.ModelForm):
     class Meta:
         model = Task
         fields = ['name', 'description', 'status', 'executor', 'labels']
-    
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        # Querysets
         self.fields['executor'].queryset = User.objects.all()
         self.fields['status'].queryset = Status.objects.all()
         self.fields['labels'].queryset = Label.objects.all()
+        # label для select по исполнителю
+        self.fields['executor'].label_from_instance = lambda obj: obj.username
