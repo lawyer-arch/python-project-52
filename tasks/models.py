7a4ffeb5615b70d8e6eabb47c88d6d568c4370bb
Pavel Anikin <PavelDidko@gmail.com>
2025-11-03 16:25:30 +0300

    Внес изменения в шаблон фильтрации заменил на value, name

--- tasks/models.py
+++ tasks/models.py
@@ -21,7 +21,7 @@ class Task(models.Model):
         null=True,
         blank=True,
         verbose_name=_("Исполнитель"),
-        related_name='Исполнитель'
+        related_name='tasks_executed'
     )
     status = models.ForeignKey(
         'statuses.Status',
